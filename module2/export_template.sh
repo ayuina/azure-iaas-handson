615af02fdae4d2eabe18ab3f22402ced76f61f0e
Ayumu Inaba <ainaba@microsoft.com>
2018-12-27 17:32:29 +0900

    エクスポートスクリプトを修正

--- module2/export_template.sh
+++ module2/export_template.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-az group export --resource-group $MOD01_RG > "${PREFIX}-template.json"
+az group export \
+    --resource-group $MOD01_RG \
+    --include-parameter-default-value \
+    > "${MOD01_RG}-exported-template.json"
 
