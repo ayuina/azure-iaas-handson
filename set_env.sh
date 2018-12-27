864bbad8b203eaa5652c6b99453fd39ce8f5684e
Ayumu Inaba <ainaba@microsoft.com>
2018-12-27 15:06:51 +0900

    スクリプトを移動

--- set_env.sh
+++ set_env.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 export PREFIX="yourname"
+
 export MOD01_RG="${PREFIX}-mod01-rg"
+export MOD02_RG="${PREFIX}-mod02-rg"
+export MOD03_RG="${PREFIX}-mod03-rg"
 export REGION="japaneast"
+
 
