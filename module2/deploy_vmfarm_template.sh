9ab3acf59219d8e7eaeba6ffeaacf94c86b53421
Ayumu Inaba <ayumu.inaba@gmail.com>
2018-12-28 02:45:31 +0000

    debug script

--- module2/deploy_vmfarm_template.sh
+++ module2/deploy_vmfarm_template.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-SUBSCRIPTION_ID=$(az account show --query id)
+SUBSCRIPTION_ID=$(az account show --query id | sed -e 's/\"//g')
 VNET_NAME="${PREFIX}-vnet"
 SUBNET_NAME="vmfarm"
 SUBNET_ID="/subscriptions/${SUBSCRIPTION_ID}/resourceGroups/${MOD01_RG}/providers/Microsoft.Network/virtualNetworks/${VNET_NAME}/subnets/${SUBNET_NAME}"
 
-az group deployment validate \
+az group deployment create \
     --resource-group $MOD02_RG \
     --template-file "vmfarm_template.json" \
     --parameters prefix="${PREFIX}" location="${REGION}" subnetId="${SUBNET_ID}"
+
