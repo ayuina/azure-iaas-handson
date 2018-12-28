97a1776901f9a691033c712bbf3a5576f2b39590
Ayumu Inaba <ayumu.inaba@gmail.com>
2018-12-28 00:39:53 +0000

    add subnet for module 2

--- module2/add_subnet.sh
+++ module2/add_subnet.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-az group create --name $MOD02_RG --location $REGION
+VNET_NAME="${PREFIX}-vnet"
+SUBNET_NAME="vmfarm"
+
+az network vnet subnet create \
+    --resource-group $MOD01_RG \
+    --vnet-name $VNET_NAME \
+    --name $SUBNET_NAME \
+    --address-prefixes "10.12.26.64/26" \
+    --output table
 
